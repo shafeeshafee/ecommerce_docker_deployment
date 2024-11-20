d0e32f4cbbb238ada8140de4666eaf8c1239b1f3
shafeeshafee <shafeelinks@gmail.com>
2024-11-20 11:56:46 +0000

    update backend container configuration and initialization

--- Terraform/modules/compute/init/backend-init.sh
+++ Terraform/modules/compute/init/backend-init.sh
@@ -22,14 +22,5 @@ python manage.py migrate payments
 echo 'Creating superuser...'
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else print('Superuser already exists')" | python manage.py shell
 
-echo 'Loading initial data if needed...'
-if [ ! -f /app/initial_data_loaded ]; then
-    echo 'Dumping data from SQLite...'
-    python manage.py dumpdata --database=sqlite --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 4 > datadump.json
-    echo 'Loading data into PostgreSQL...'
-    python manage.py loaddata datadump.json
-    touch /app/initial_data_loaded
-fi
-
 echo 'Starting server...'
 python manage.py runserver 0.0.0.0:8000
